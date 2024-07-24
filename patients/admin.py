4396e97f79c9bde9e4e6dfa4eeedba70e5897665
zila-tech <info.zilatech@gmail.com>
2024-07-24 04:52:03 +0000

    Update patients admin, register questionaire-response and response model

--- patients/admin.py
+++ patients/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from patients.models import QuestionnaireResponse, Response
+
 # Register your models here.
+admin.site.register(QuestionnaireResponse)
+admin.site.register(Response)
