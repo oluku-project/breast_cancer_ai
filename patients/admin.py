ebde9e72f1c34a04493cf5411b8127b2d2c2841e
zila-tech <info.zilatech@gmail.com>
2024-07-24 17:27:19 +0000

    Register PredictionResult in patients admin page

--- patients/admin.py
+++ patients/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from patients.models import QuestionnaireResponse, Response
+from patients.models import QuestionnaireResponse, Response, PredictionResult
 
 # Register your models here.
 admin.site.register(QuestionnaireResponse)
 admin.site.register(Response)
+admin.site.register(PredictionResult)
