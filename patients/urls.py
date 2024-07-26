8bb007b09440bb292da8dd9a25cb99cb420254fd
zila-tech <info.zilatech@gmail.com>
2024-07-26 20:04:13 +0000

    Update patients app url pattern with pending result, delete pendinding results paths

--- patients/urls.py
+++ patients/urls.py
@@ -48,6 +48,8 @@ urlpatterns = [
     # Done with the below urls
     path("questionnaire/", questionnaier, name="questionnaire"),
     path("questionnaire/update/<int:pk>/", questionnaier, name="update_questionnaire"),
+    path("pending/results/", pending_result, name="pending_results"),
+    path("delete/pending-results/", pending_result_delete, name="delete_pending_result"),
     path("summary/<pk>/", summary_view, name="summary"),
     path("result/<pk>/", results, name="result"),
 ]
