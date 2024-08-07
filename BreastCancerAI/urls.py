270945970afdef34c32965f7abdbcce0665de09a
zila-tech <info.zilatech@gmail.com>
2024-08-07 03:39:11 +0000

    Remove login, logout, activate url path for main project urls.py

--- BreastCancerAI/urls.py
+++ BreastCancerAI/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import *
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,9 +7,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("patients.urls")),
     path("auth/", include("accounts.urls")),
-    path("login/", login, name="login"),
-    path("logout/", logout, name="logout"),
-    path("activate/<uidb64>/<token>/", activate, name="activate"),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
