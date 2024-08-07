3970208d26f12701830861f689d97bcbe61f3718
zila-tech <info.zilatech@gmail.com>
2024-08-07 03:50:58 +0000

    Clear patients app urls.py

--- patients/urls.py
+++ patients/urls.py
@@ -3,47 +3,7 @@ from .views import *
 
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("", index, name="home"),
-    path("profile/", profile, name="profile"),  # done
-    path("user-dashboard/", user_dashboard, name="user_dashboard"),  # done
-    path("user-profile/", user_profile, name="user_profile"),  # done
-    # path("contact/", contact, name="contact"),
-    path("export/", export_data, name="export_data"),
-    path("import/", import_data, name="import_data"),
-    path("data/", data_export_import, name="data_export_import"),
-    path("admin-dashboard/", admin_dashboard, name="admin_dashboard"),
-    path(
-        "admin-user-management/",
-        admin_user_management,
-        name="admin_user_management",
-    ),
-    path(
-        "admin-questionnaire-management/",
-        admin_questionnaire_management,
-        name="admin_questionnaire_management",
-    ),
-    path(
-        "admin-data-visualization/",
-        admin_data_visualization,
-        name="admin_data_visualization",
-    ),
-    path(
-        "admin-record-management/",
-        admin_record_management,
-        name="admin_record_management",
-    ),
-    path(
-        "admin-system-settings/",
-        admin_system_settings,
-        name="admin_system_settings",
-    ),
-    path("admin-reports/", admin_reports, name="admin_reports"),
-    path("contacts/", contacts, name="contacts"),
-    path("record-details/", record_details, name="record_details"),
-    path("result-hostores/", result_hostores, name="result_hostores"),
-    path("visualization/", charts_and_data_visualization, name="visualization"),
-    # Done with the below urls
+    path("", homeview, name="home"),
     path("questionnaire/", questionnaier, name="questionnaire"),
     path("questionnaire/update/<int:pk>/", questionnaier, name="update_questionnaire"),
     path("pending/results/", pending_result, name="pending_results"),
@@ -52,8 +12,11 @@ urlpatterns = [
     ),
     path("summary/<pk>/", summary_view, name="summary"),
     path("result/<pk>/", results, name="result"),
+    path("result-hostores/", result_hostores, name="result_hostores"),
     path("delete-result/", resultdelete_view, name="delete_result"),
     path("detailed/result/<pk>/", results, name="detailed_result"),
+    path("report/download/", pdfreportdownload, name="report_download"),
+    path("report/print/", pdfreportprint, name="report_print"),
     path("feedback/", feedback, name="feedback"),
     path("contact/", contactview, name="contact"),
     path("about/", about, name="about"),
